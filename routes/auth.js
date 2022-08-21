800388a1eb4e03f578868f0f7870a283f9873fda
vyashya12 <vyashyaraj25@gmail.com>
2022-08-21 15:03:22 +0800

    Login and get done

--- routes/auth.js
+++ routes/auth.js
@@ -35,4 +35,34 @@ router.post("/register", (req, res) => {
   })
 })
 
+router.post("/login", (req, res) => {
+  const { fullname, password } = req.body
+  if (!fullname || !password) {
+    return res.json({ message: "Cannot have empty fields" })
+  }
+  User.findOne({ fullname }, (err, user) => {
+    if (!user) return res.status(400).json({ message: "User does not exist" })
+    if (err) return res.status(400).json({ err })
+
+    let isMatch = bcrypt.compareSync(password, user.password)
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password" })
+    } else {
+      return res
+        .status(200)
+        .json({ message: "Logged in successfully", nric: user.nric })
+    }
+  })
+})
+
+router.get("/", (req, res) => {
+  const { nric } = req.body
+  User.findOne({ nric }, (err, user) => {
+    if (!user) return res.json({ message: "User does not exist" })
+    if (err) return res.json({ err })
+    if (user) return res.json({ user })
+  })
+})
+
 module.exports = router
